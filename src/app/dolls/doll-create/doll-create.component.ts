d867d2808fe84010119eec9e56b1a314bca59e0e
Juan Felipe J.Z.. Zea Sanchez <juan.zea@db-system.corp>
2025-08-19 15:30:11 -0500

    Crear Doll

--- src/app/dolls/doll-create/doll-create.component.ts
+++ src/app/dolls/doll-create/doll-create.component.ts
@@ -1,11 +1,44 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { Router } from '@angular/router';
+
+import { DollService } from '../../services/doll.service';
+import { CommonResponseService } from '../../services/common-response.service';
+
+import { DollFormComponent } from '../doll-form/doll-form.component';
+
+import { Toast } from 'primeng/toast';
+import { MessageService } from 'primeng/api';
 
 @Component({
   selector: 'app-doll-create',
-  imports: [],
+  imports: [
+    DollFormComponent,
+    Toast,
+  ],
+  providers: [MessageService],
   templateUrl: './doll-create.component.html',
   styleUrl: './doll-create.component.css'
 })
 export class DollCreateComponent {
+  private dollService: DollService = inject(DollService);
+  private commonResponseService = inject(CommonResponseService);
+  private messageService = inject(MessageService)
+  private router = inject(Router)
 
+  onSubmit(formData: any)
+  {
+    console.dir(formData);
+
+    this.dollService.createDoll(formData)
+      .subscribe({
+        next: (response: any) => {
+          console.log(response);
+
+          this.router.navigate(["/dolls"])
+        },
+        error: (error) => {
+          this.messageService.addAll(this.commonResponseService.setToastErrorMessage(error));
+        }
+      })
+  }
 }
--- src/app/dolls/doll-form/doll-form.component.ts
+++ src/app/dolls/doll-form/doll-form.component.ts
@@ -1,11 +1,83 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, inject, Input, Output, SimpleChanges } from '@angular/core';
+
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
+
+import { CardModule } from 'primeng/card';
+import { ButtonModule } from 'primeng/button';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-doll-form',
-  imports: [],
+  imports: [
+    ReactiveFormsModule,
+    CardModule,
+    ButtonModule
+  ],
   templateUrl: './doll-form.component.html',
   styleUrl: './doll-form.component.css'
 })
 export class DollFormComponent {
+  @Input() titulo: string = '';
+  @Input() doll: any;
 
+  @Output() eventEmitter = new EventEmitter();
+  errors: any[] = [];
+
+  private router: Router = inject(Router)
+
+  private fb: FormBuilder = inject(FormBuilder)
+
+  dollForm = this.fb.group({
+    nombre: ['', Validators.required],
+    edad: ['', Validators.required],
+  });
+
+  onSubmit ()
+  {
+    this.errors = [];
+
+    if (!this.dollForm.valid)
+      return;
+
+    if (this.nombre == null || this.nombre.value == null || this.nombre.value == undefined)
+      this.errors.push("El nombre es requerido");
+    if (this.edad == null || this.edad.value == null || this.edad.value == undefined)
+      this.errors.push("La edad es requerida");
+
+    if (isNaN(Number(this.edad?.value)))
+      this.errors.push('La edad debe ser un número');
+    if ((Number(this.edad?.value)) <= 0)
+      this.errors.push('La edad debe ser mayor a cero');
+
+    if (this.errors.length > 0)
+      return;
+
+    let formData = {
+      nombre: this.nombre?.value,
+      edad: this.edad?.value,
+    }
+
+    this.eventEmitter.emit(formData);
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    if (changes["doll"] === null || changes["doll"] === undefined 
+      || changes["doll"].currentValue === null || changes["doll"].currentValue === undefined)
+    {
+      return;
+    }
+
+    this.doll = changes["doll"].currentValue;
+
+    this.dollForm.controls.nombre.setValue(this.doll.nombre);
+    this.dollForm.controls.edad.setValue(this.doll.ciudad);
+  }
+
+  regresarDolls()
+  {
+    this.router.navigate(['/dolls'])
+  }
+
+  get nombre() { return this.dollForm.get('nombre'); }
+  get edad() { return this.dollForm.get('edad'); }
 }
